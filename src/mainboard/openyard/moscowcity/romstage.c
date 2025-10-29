1aebd4a49a79940b933354e7e12b0c37a752ba32
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2025-10-29 14:03:25 +0000

    Changes: 		Functions ht_setup(), iio_setup(), sata_setup(mupd) are called during FSPM_CONFIG setting up; 		Function timeout_setup() was implemented and is called to setup timeout value before starting to boot, 		the function is as a simple example of EFI variables usage in coreboot.

--- src/mainboard/openyard/moscowcity/romstage.c
+++ src/mainboard/openyard/moscowcity/romstage.c
@@ -16,8 +16,9 @@
 #include <types.h>
 #include <fmap.h>
 
-#define VAR_SIZE 0x10
-
+#include <ht_config.h>
+#include <iio_config.h>
+#include <sata_config.h>
 
 static void setup_gpio(void) {
 #define PID_GPIOCOM0   0x6E
@@ -95,148 +96,40 @@ static void mainboard_config_iio(FSPM_UPD *mupd)
 }
 
 
-static void read_variable (void) {
-    char var_name[1024] = "Timeout";
-    uint32_t var_size = VAR_SIZE;
-    uint8_t var_data[VAR_SIZE];
-    struct region_device smmstore;
+static int32_t timeout_setup (void) {
+	char var_name[1024] = "Timeout";
+	const EFI_GUID var_guid = { 0x8BE4DF61, 0x93CA, 0x11d2, {0xAA, 0x0D, 0x00, 0xE0, 0x98, 0x03, 0x2B, 0x8C} };
+	uint16_t timeout;
+	uint32_t timeout_size = sizeof(timeout);
+	struct region_device smmstore;
 
-    const EFI_GUID EfiVariableGuid = { 0x8BE4DF61, 0x93CA, 0x11d2, {0xAA, 0x0D, 0x00, 0xE0, 0x98, 0x03, 0x2B, 0x8C} };
+	if (fmap_locate_area_as_rdev_rw("SMMSTORE", &smmstore) < 0) { // src/lib/fmap.c
+		printk(BIOS_ERR, "SMMSTORE region wasn't found \n");
+		return -1;
+	}
 
-    const EFI_GUID * pguid;
+	enum cb_err error = efi_fv_get_option(&smmstore, &var_guid, var_name, &timeout, &timeout_size);
+	if (error != 0) {
+		printk(BIOS_WARNING, "EFI variable %s can't be gotten, error: %d\n", var_name, error);
+		*(uint16_t*)&timeout = 0x0A;
+	} else {
+		printk(BIOS_DEBUG, "Timeout: 0x%04X, size: %d\n", *(uint16_t*)&timeout, timeout_size);
+	}
 
-    if (fmap_locate_area_as_rdev_rw("SMMSTORE", &smmstore) < 0) { // src/lib/fmap.c
-				printk(BIOS_ERR, "SMMSTORE region wasn't found \n");
-				return;
-    }
-		/* Timeout */
-    enum cb_err error = efi_fv_get_option(&smmstore, &EfiVariableGuid, var_name, &var_data, &var_size);
-    if (error != 0) {
-				printk(BIOS_ERR, "EFI variable %s can't be gotten, error: %d\n", var_name, error);
-		  	var_data[0] = 0x05;
-    		var_data[1] = 0x00;
+	if (fmap_locate_area_as_rdev_rw("SMMSTORE", &smmstore) < 0) { // src/lib/fmap.c
+		printk(BIOS_ERR, "SMMSTORE region wasn't found \n");
+		return -2;
+	}
 
+	error = efi_fv_set_option(&smmstore, &var_guid, var_name, &timeout, timeout_size);
 
-    } else {
-			printk(BIOS_ERR, "Timeout: 0x%02X, size: %d\n", *(uint16_t*)&var_data, var_size);
-//	    var_data[0]++;
-//	    var_data[1]++;
-    }
-
-
-    if (fmap_locate_area_as_rdev_rw("SMMSTORE", &smmstore) < 0) { // src/lib/fmap.c
-				printk(BIOS_ERR, "SMMSTORE region wasn't found \n");
-				return;
-    }
-		error = efi_fv_set_option(&smmstore, &EfiVariableGuid, var_name, &var_data, var_size);
-
-		if (error != 0)
-    	printk(BIOS_ERR, "EFI variable %s can't be set up, error: %d\n", var_name, error);
-
-		/* OySocketProcessorCoreConfig */    
-		const EFI_GUID gEfiOySocketProcessorCoreVarGuid = { 0x07013588, 0xC789, 0x4E12, { 0xA7, 0xC3, 0x88, 0xFA, 0xFA, 0xE7, 0x9F, 0xC7 } };
-		strcpy(var_name, "OySocketProcessorCoreConfig");
-    pguid = &gEfiOySocketProcessorCoreVarGuid;
-    OY_SOCKET_PROCESSORCORE_CONFIGURATION soc_proc_config;
-    uint32_t soc_proc_config_size = sizeof(OY_SOCKET_PROCESSORCORE_CONFIGURATION);
-
-    if (fmap_locate_area_as_rdev_rw("SMMSTORE", &smmstore) < 0) { // src/lib/fmap.c
-				printk(BIOS_ERR, "SMMSTORE region wasn't found \n");
-				return;
-    }
-    error = efi_fv_get_option(&smmstore, pguid, var_name, &soc_proc_config, &soc_proc_config_size);
-    if (error != 0) {
-				printk(BIOS_ERR, "EFI variable %s can't be gotten, error: %d\n", var_name, error);
-				soc_proc_config.ProcessorHyperThreadingDisable = 1;
-    } else {
-        printk(BIOS_ERR, "Socket processor configuration:\n");
-				printk(BIOS_ERR, "ProcessorHyperThreadingDisable: 0x%01X\n", soc_proc_config.ProcessorHyperThreadingDisable);
-    }
-
-   	if (fmap_locate_area_as_rdev_rw("SMMSTORE", &smmstore) < 0) { // src/lib/fmap.c
-				printk(BIOS_ERR, "SMMSTORE region wasn't found \n");
-				return;
-		}
-		error = efi_fv_set_option(&smmstore, pguid, var_name, &soc_proc_config, soc_proc_config_size);
-		if (error != 0)
-			printk(BIOS_ERR, "EFI variable %s can't be set up, error: %d\n", var_name, error);
-
-    /* OySocketIioeConfig */
-    const EFI_GUID gEfiOySocketIioVariableGuid = { 0xdd84017e, 0x7f52, 0x48f9, { 0xb1, 0x6e, 0x50, 0xed, 0x9e, 0x0d, 0xbe, 0x72 }  };
-    strcpy(var_name, "OySocketIioConfig");
-    pguid = &gEfiOySocketIioVariableGuid;
-    OY_SOCKET_IIO_CONFIGURATION soc_iio_config;
-    uint32_t soc_iio_config_size = sizeof(OY_SOCKET_IIO_CONFIGURATION);
-      
-    if (fmap_locate_area_as_rdev_rw("SMMSTORE", &smmstore) < 0) { // src/lib/fmap.c
-        printk(BIOS_ERR, "SMMSTORE region wasn't found \n");
-        return;
-    }
-    error = efi_fv_get_option(&smmstore, pguid, var_name, &soc_iio_config, &soc_iio_config_size);
-    if (error != 0) {
-        printk(BIOS_ERR, "EFI variable %s can't be gotten, error: %d\n", var_name, error);
-        soc_iio_config.ConfigIOU0[0] = 0x0;
-				soc_iio_config.ConfigIOU0[1] = 0x0;
-				soc_iio_config.ConfigIOU1[0] = 0x0;
-				soc_iio_config.ConfigIOU1[1] = 0x0;
-				soc_iio_config.ConfigIOU2[0] = 0x0;
-				soc_iio_config.ConfigIOU2[1] = 0x0;
-				soc_iio_config.ConfigIOU3[0] = 0x0;
-				soc_iio_config.ConfigIOU3[1] = 0x0;
-				soc_iio_config.ConfigIOU4[0] = 0x0;
-				soc_iio_config.ConfigIOU4[1] = 0x0;
-				soc_iio_config.ConfigIOU5[0] = 0x0;
-				soc_iio_config.ConfigIOU5[1] = 0x0;
-				soc_iio_config.ConfigIOU6[0] = 0x0;
-				soc_iio_config.ConfigIOU6[1] = 0x0;
-				soc_iio_config.ConfigIOU7[0] = 0x0;
-				soc_iio_config.ConfigIOU7[1] = 0x0;
-    } else {
-        printk(BIOS_ERR, "Socket processor configuration:\n");
-        printk(BIOS_ERR, "ConfigIOU0: 0x%02X\n", *(UINT16 *)&soc_iio_config.ConfigIOU0);
-    }      
-  
-    if (fmap_locate_area_as_rdev_rw("SMMSTORE", &smmstore) < 0) { // src/lib/fmap.c
-        printk(BIOS_ERR, "SMMSTORE region wasn't found \n");
-        return;
-    }
-    error = efi_fv_set_option(&smmstore, pguid, var_name, &soc_iio_config, soc_iio_config_size);
-    if (error != 0)
-      printk(BIOS_ERR, "EFI variable %s can't be set up, error: %d\n", var_name, error);
-	
-
-		/* SataSetupConfig */    
-		const EFI_GUID gEfiOySataSetupVariableGuid = {0xD12BFF13, 0x6225, 0x44C4, {0xBE, 0x3F, 0x29, 0xE8, 0x32, 0xE6, 0x87, 0x41}/*0x8BE4DF61, 0x93CA, 0x11d2, {0xAA, 0x0D, 0x00, 0xE0, 0x98, 0x03, 0x2B, 0x8C}*/ };
-		strcpy(var_name, "SataSetup");
-    pguid = &gEfiOySataSetupVariableGuid;
-    SATA_SETUP_CONFIGURATION sata_config;
-    uint32_t sata_config_size = sizeof(SATA_SETUP_CONFIGURATION);
-    if (fmap_locate_area_as_rdev_rw("SMMSTORE", &smmstore) < 0) { // src/lib/fmap.c
-        printk(BIOS_ERR, "SMMSTORE region wasn't found \n");
-        return;
-    }
-    error = efi_fv_get_option(&smmstore, pguid, var_name, &sata_config, &sata_config_size);
-    if (error != 0) {
-				printk(BIOS_ERR, "EFI variable %s can't be gotten, error: %d\n", var_name, error);
-				sata_config.Port1 = 0x1;
-				sata_config.Port2 = 0x1;
-    } else {
-        printk(BIOS_ERR, "SATA configuration:\n");
-				printk(BIOS_ERR, "Port1: 0x%02X\n", sata_config.Port1);
-    }
-
-    if (fmap_locate_area_as_rdev_rw("SMMSTORE", &smmstore) < 0) { // src/lib/fmap.c
-		   	printk(BIOS_ERR, "SMMSTORE region wasn't found \n");
-				return;
-    }
-		error = efi_fv_set_option(&smmstore, pguid, var_name, &sata_config, sata_config_size);
-		if (error != 0)
-			printk(BIOS_ERR, "EFI variable %s can't be set up, error: %d\n", var_name, error);
+	if (error != 0)
+		printk(BIOS_ERR, "EFI variable %s can't be set up, error: %d\n", var_name, error);
+	return 0;
 }
 
 void mainboard_memory_init_params(FSPM_UPD *mupd)
 {
-	read_variable();
 	/* Setup FSP log */
 	if (CONFIG(OCP_VPD)) {
 		mupd->FspmConfig.SerialIoUartDebugEnable = get_bool_from_vpd(FSP_LOG,
@@ -277,4 +170,14 @@ void mainboard_memory_init_params(FSPM_UPD *mupd)
 	soc_config_iio(mupd, ac_iio_pci_port, ac_iio_bifur);
 	mainboard_config_iio(mupd);
 	setup_gpio();
+	ht_setup(mupd);
+	iio_setup(mupd);
+	timeout_setup();
+	sata_setup(mupd);
+
+// Setup MMCFG Base Address - 0: 1G, 1: 1.5G, 2: 1.75G, 3: 2G, 4: 2.25G, 5: 3G, 6: Auto(Default)
+	printk(BIOS_DEBUG, "mupd->FspmConfig.mmCfgBase: 0x%02X\n", mupd->FspmConfig.mmCfgBase);
+// Select MMCFG Size - 0: 64M, 1: 128M, 2: 256M, 3: 512M, 4: 1G, 5: 2G, 6: Auto(Default).
+	printk(BIOS_DEBUG, "mupd->FspmConfig.mmCfgSize: 0x%02X\n", mupd->FspmConfig.mmCfgSize);
+
 }
